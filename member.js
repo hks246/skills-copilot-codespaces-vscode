5f08b27aab1787a7d87b9d8733b35f086875486d
scalingcloud <scalingcloud@gmail.com>
2023-08-20 23:38:25 -0400

    CoPilot second page

--- member.js
+++ member.js
@@ -1,22 +1,8 @@
-function skillsMember() {
-    var member = document.getElementById('member');
-    var skills = document.getElementById('skills');
-    var projects = document.getElementById('projects');
-    var contact = document.getElementById('contact');
-    var memberContent = document.getElementById('memberContent');
-    var skillsContent = document.getElementById('skillsContent');
-    var projectsContent = document.getElementById('projectsContent');
-    var contactContent = document.getElementById('contactContent');
-    memberContent.style.display = 'block';
-    skillsContent.style.display = 'none';
-    projectsContent.style.display = 'none';
-    contactContent.style.display = 'none';
-    member.style.color = '#fff';
-    skills.style.color = '#000';
-    projects.style.color = '#000';
-    contact.style.color = '#000';
-    member.style.backgroundColor = '#000';
-    skills.style.backgroundColor = '#fff';
-    projects.style.backgroundColor = '#fff';
-    contact.style.backgroundColor = '#fff';
+function members() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/members.html',
+    controller: 'MembersCtrl',
+    controllerAs: 'membersCtrl'
+  };
 }
